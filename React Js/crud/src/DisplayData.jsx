b7d054a19ff85ed04adefe9cda88527887536869
boyellaanil <boyellaanil924@gmail.com>
2025-11-21 19:16:53 +0530

    Update

--- React Js/crud/src/DisplayData.jsx
+++ React Js/crud/src/DisplayData.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const DisplayData = ({details}) => {
+const DisplayData = ({ details, handleEdit, handleDelete }) => {
   return (
     <div>
-       {details.map((value) => {
+      {details.map((value) => {
         return (
           <article>
             <p>Product Name: {value.productName}</p>
             <p>Product Price: {value.productPrice}</p>
             <p>Product Description: {value.productDescription}</p>
             <aside>
-              <button>Edit</button>
-              <button>Delete</button>
+              <button
+                onClick={() => {
+                  handleEdit(value);
+                }}
+              >
+                Edit
+              </button>
+              <button onClick={() => handleDelete(value)}>Delete</button>
             </aside>
           </article>
         );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default DisplayData
+export default DisplayData;
--- React Js/crud/src/Parent.jsx
+++ React Js/crud/src/Parent.jsx
@@ -1,40 +1,64 @@
-import React,{useState} from 'react'
-import Form from './Form.jsx';
-import DisplayData from './DisplayData.jsx';
+import React, { useState } from "react";
+import Form from "./Form.jsx";
+import DisplayData from "./DisplayData.jsx";
 import "./style.css";
+import { v4 as uuidv4 } from "uuid";
 const Parent = () => {
-    let [details, setDetails] = React.useState({
-        productName: "",
-        productPrice: "",
-        productDescription: "",
-    });
-    let [displayData, setDisplayData] = useState([]);
-    let handleChange = (e) => {
-        let {name, value} = e.target;
-        setDetails({...details, [name]: value});
-    };
-    let handleSubmit = (e) => {
+  let [details, setDetails] = React.useState({
+    id: uuidv4(),
+    productName: "",
+    productPrice: "",
+    productDescription: "",
+  });
+  let [displayData, setDisplayData] = useState([]);
+
+  let handleChange = (e) => {
+    let { name, value } = e.target;
+    setDetails({ ...details, [name]: value });
+  };
+
+  let handleSubmit = (e) => {
     e.preventDefault();
     setDisplayData([...displayData, details]);
     setDetails({
+      id: uuidv4(),
       productName: "",
       productPrice: "",
       productDescription: "",
     });
   };
+  console.log(displayData);
+  let updateproduct = (data) => {
+    setDetails(data);
+    let filteredData = displayData.filter((value) => {
+      value.id != data.id;
+    });
+    setDisplayData(filteredData);
+  };
+  let handleDelete = (data) => {
+    let filteredData = displayData.filter((value) => {
+      value.id != data.id;
+    });
+    setDisplayData(filteredData);
+  };
   return (
-    <div className='parent-container'>
-        <aside>
-            <Form details={details} 
-            handleChange={handleChange}
-            handleSubmit={handleSubmit}
-            />
-        </aside>
-        <aside>
-            <DisplayData details={displayData}/>
-        </aside>
+    <div className="parent-container">
+      <aside>
+        <Form
+          details={details}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+      </aside>
+      <aside>
+        <DisplayData
+          details={displayData}
+          handleEdit={updateproduct}
+          handleDelete={handleDelete}
+        />
+      </aside>
     </div>
-  )
-}
+  );
+};
 
-export default Parent
+export default Parent;
