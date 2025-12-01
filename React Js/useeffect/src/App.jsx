607737f5c520b5a9f960f70797f152270752a78d
boyellaanil <boyellaanil924@gmail.com>
2025-12-01 19:37:02 +0530

    update

--- React Js/useeffect/src/App.jsx
+++ React Js/useeffect/src/App.jsx
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import Child from './Child';
 
-//! useEffect will accept the two parameters 1.functions ,2 .dependencies
+// ! useEffect will accept the two parameters 1.functions ,2 .dependencies
 const App = () => {
     let [count,setCount]=useState(0);
 
-    //!it will behaves like a render method
-    useEffect(()=>{
-        console.log("i am use render method")
-    });
+    // ! it will behaves like a render method
+    // useEffect(()=>{
+    //     console.log("i am use render method")
+    // });
 
     //! below behaves like componentDidMount
-     useEffect(()=>{
-        console.log("i am a component did mount")
-    },[]);
+    //  useEffect(()=>{
+    //     console.log("i am a component did mount")
+    // },[]);
 
     let handleClick=()=>{
         setCount(count+1);
     }
 
     //! below behaves like componentDidUpdate
-     useEffect(()=>{
-        console.log("i am a component did Update")
-    },[count]);
-    
+    //  useEffect(()=>{
+    //     console.log("i am a component did Update")
+    // },[count]);
+
   return (
     <div>
-        {count<=5 && <h1>{count}</h1>}
+        {count<=5 && <Child count={count} handleClick={handleClick}/>}
     </div>
   )
 }
