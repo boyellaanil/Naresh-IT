b958c8b0b7e5f10c70fec81592f49929e2bab25b
boyellaanil <boyellaanil924@gmail.com>
2025-12-15 21:46:07 +0530

    update

--- React Js/Assignments/eduport/src/components/trainers/CreateTrainer.jsx
+++ React Js/Assignments/eduport/src/components/trainers/CreateTrainer.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import { trainerContext } from './../context/TrainerContext';
+import CreateForm from './../form/CreateForm';
 const CreateTrainer = () => {
-  return (
-    <div>CreateTrainer</div>
-  )
-}
+  let [trainerDetails, setTrainerDetails] = useState({
+    name: "",
+    experience: "",
+    technologies: "",
+    email: "",
+    contact: "",
+  });
+  let { name, experience, technologies, email, contact } = trainerDetails;
+  let {addTrainer}=useContext(trainerContext)
+  let trainerData = [
+    {
+      name: "name",
+      stateValue: name,
+      type: "text",
+    },
+    {
+      name: "email",
+      stateValue: email,
+      type: "email",
+    },
+    {
+      name: "contact",
+      stateValue: contact,
+      type: "tel",
+    },
+    {
+      name: "experience",
+      stateValue: experience,
+      type: "number",
+    },
+    {
+      name: "technologies",
+      stateValue: technologies,
+      type: "text",
+    },
+  ];
+  let handleChange = (e) => {
+    let { name, value } = e.target;
+    setTrainerDetails({ ...trainerDetails, [name]: value });
+  };
+  let handleSubmit = () => {
+    addTrainer(trainerDetails);
+  };
 
-export default CreateTrainer
+  return <div>
+    <CreateForm data={trainerData} handleChange={handleChange} handleSubmit={handleSubmit}/>
+  </div>
+};
+
+export default CreateTrainer;
--- React Js/Assignments/eduport/src/main.jsx
+++ React Js/Assignments/eduport/src/main.jsx
@@ -5,13 +5,16 @@ import { RouterProvider } from "react-router-dom";
 import route from "./components/routes/Route";
 import AuthProvider from "./components/context/AuthContext";
 import CousreProvider from "./components/context/CourseContext";
+import TrainerProvider from "./components/context/TrainerContext";
 
 createRoot(document.getElementById("root")).render(
-  <CousreProvider>
-    <AuthProvider>
-      <RouterProvider router={route}>
-        <App />
-      </RouterProvider>
-    </AuthProvider>
-  </CousreProvider>
+  <TrainerProvider>
+    <CousreProvider>
+      <AuthProvider>
+        <RouterProvider router={route}>
+          <App />
+        </RouterProvider>
+      </AuthProvider>
+    </CousreProvider>
+  </TrainerProvider>
 );
