084e539df78178313bf7c206cc6b5cc082b06807
boyellaanil <boyellaanil924@gmail.com>
2025-12-15 21:52:59 +0530

    update

--- React Js/Assignments/eduport/src/components/trainers/DisplayTrainer.jsx
+++ React Js/Assignments/eduport/src/components/trainers/DisplayTrainer.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { trainerContext } from './../context/TrainerContext';
+import { useNavigate } from 'react-router-dom';
 
 const DisplayTrainer = () => {
+  let {fetchTrainersData,trainersData}=useContext(trainerContext);
+  useEffect(()=>{
+    fetchTrainersData();
+  },[]);
+
+  let navigate=useNavigate();
   return (
-    <div>DisplayTrainer</div>
+    <div>
+      {trainersData.map((value)=>{
+        return (
+          <article>
+            <p>TrainerName :{value.name}</p>
+            <p>Technology : {value.technologies}</p>
+            <button onClick={()=>{
+              navigate(`/singleTainer/${value.id}`)
+            }}>View Details</button>
+          </article>
+        )
+      })}
+    </div>
   )
 }
 
