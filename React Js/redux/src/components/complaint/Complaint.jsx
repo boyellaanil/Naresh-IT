5e6a60ee0af1ef5ecc73a4308edd4535b51fe756
boyellaanil <boyellaanil924@gmail.com>
2025-12-23 22:11:11 +0530

    update

--- React Js/redux/src/components/complaint/Complaint.jsx
+++ React Js/redux/src/components/complaint/Complaint.jsx
@@ -1,29 +1,67 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { addComplaint } from '../redux/slices/ComplaintSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addComplaint,
+  deleteComplaint,
+  editComplaints,
+} from "../redux/slices/ComplaintSlice";
 
 const Complaint = () => {
-    let [complaintData,setComplaintData]=useState("");
-    let complaints=useSelector(state=>state.complaint.complaints);
-    let dispatch=useDispatch();
-    let handleChange=(e)=>{
-        setComplaintData(e.target.value);
-    };
-    let handleSubmit=(e)=>{
-        e.preventDefault();
-        dispatch(addComplaint(complaintData));
-        console.log(complaints);
-    };
+  let [complaintData, setComplaintData] = useState({
+    id: crypto.randomUUID(),
+    complaintValue: "",
+  });
+  let complaints = useSelector((state) => state.complaint.complaints);
+  let singleComplaint = useSelector((state) => state.complaint.singleComplaint);
+  let dispatch = useDispatch();
+  let handleChange = (e) => {
+    setComplaintData({ ...complaintData, complaintValue: e.target.value });
+  };
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addComplaint(complaintData));
     console.log(complaints);
+    setComplaintData({
+      id: crypto.randomUUID(),
+      complaintValue: "",
+    });
+  };
+  useEffect(() => {
+    setComplaintData({
+      ...complaintData,
+      complaintValue: singleComplaint.complaintValue,
+    });
+  }, [singleComplaint.complaintValue]);
+  console.log(complaints);
   return (
     <div>
-        <form action=""onSubmit={handleSubmit}>
-            <label htmlFor="">Complaint</label>
-            <input type="text" onChange={handleChange} />
-            <button type="submit">Submit</button>   
-        </form>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">Complaint</label>
+        <input
+          type="text"
+          value={singleComplaint.complaintValue || complaintData.complaintValue}
+          onChange={handleChange}
+          name="complaintValue"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <section>
+        {complaints.map((value) => {
+          return (
+            <article>
+              <p>{value.complaintValue}</p>
+              <button onClick={() => dispatch(editComplaints(value.id))}>
+                Edit
+              </button>
+              <button onClick={() => dispatch(deleteComplaint(value.id))}>
+                Delete
+              </button>
+            </article>
+          );
+        })}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Complaint
+export default Complaint;
