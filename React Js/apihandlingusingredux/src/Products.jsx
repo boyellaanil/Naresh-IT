9c22c48fd915e41c2586f1f92925121470c89e83
boyellaanil <boyellaanil924@gmail.com>
2025-12-25 17:08:12 +0530

    api handling using redux update

--- React Js/apihandlingusingredux/src/Products.jsx
+++ React Js/apihandlingusingredux/src/Products.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { fetchProducts } from './redux/productSlice'
+import React, { useEffect } from "react";
+import { fetchProducts } from "./redux/productSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Products = () => {
+  let value = useSelector((state) => state.product.products);
+  console.log(value);
 
-    console.log(fetchProductsApi());
+  let dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      {value.map((product) => {
+        return <p>{product.title}</p>;
+      })}
+    </div>
+  );
+};
 
-export default Products
+export default Products;
--- React Js/apihandlingusingredux/src/redux/productSlice.js
+++ React Js/apihandlingusingredux/src/redux/productSlice.js
@@ -1,12 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-let fetchProductApi=()=>axios.get("https://fakestoreapi.com/products");
+export let fetchProductApi=()=>axios.get("https://fakestoreapi.com/products");
+
+export let fetchProducts = createAsyncThunk(
+    "products/fetchProducts",
+    async()=>{
+        let {data}=await fetchProductApi();
+        return data;
+    }
+);
 
 let productSlice=createSlice({
     name:"productSlice",
     initialState:{products:[]},
     reducers:[],
+    extraReducers:(builders)=>{
+        builders
+        .addCase(fetchProducts.fulfilled,(state,action)=>{
+            state.products =action.payload;
+        })
+    }
 });
 
 export default  productSlice.reducer;
