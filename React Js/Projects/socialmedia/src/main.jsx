6c60a82db0aa96cf29fd8bbe2bd1c604492984e2
boyellaanil <boyellaanil924@gmail.com>
2026-01-02 20:58:48 +0530

    update

--- React Js/Projects/socialmedia/src/main.jsx
+++ React Js/Projects/socialmedia/src/main.jsx
@@ -3,7 +3,8 @@ import App from "./App";
 import { RouterProvider } from "react-router-dom";
 import route from "./components/routes/Route";
 import { Provider } from "react-redux";
-import store from "./components/redux/store/store"
+import store from "./components/redux/store/store";
+import "../global.css";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
