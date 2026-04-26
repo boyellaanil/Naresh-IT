ab829d9ca286bc0ca79ecbb939a289649c42ecca
boyellaanil <boyellaanil924@gmail.com>
2026-04-26 19:56:27 +0530

    update

--- React Js/apihandlingusingredux/src/redux/product/productApi.js
+++ React Js/apihandlingusingredux/src/redux/product/productApi.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 let baseUrl="http://localhost:3000/products";
 export let fetchProductsData =axios.get(baseUrl);
 export let addproducts =(product)=>axios.post(baseUrl,product);
