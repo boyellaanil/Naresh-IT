9f6f92be9b9ad544d88bcb51cde55388a5894f32
boyellaanil <boyellaanil924@gmail.com>
2026-05-25 21:42:21 +0530

    update

--- React Js/apihandlingusingredux/src/main.jsx
+++ React Js/apihandlingusingredux/src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from './redux/store';
-
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <App/>
