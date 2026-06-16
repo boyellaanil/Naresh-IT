8f46aa73dcd61c8d1f2b048e35addd95084da280
boyellaanil <boyellaanil924@gmail.com>
2026-06-16 21:11:29 +0530

    update

--- React Js/applyimages/src/App.jsx
+++ React Js/applyimages/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./global.css";
 import Child1 from "./child1/Child1";
 import Child2 from "./child2/Child2";
-
 const App = () => {
   let cb = {
     color: "green",
